ee4ba40961021baeb4ea19b5906d4e97cd043688
Ann-lau <a.lau@alustudent.com>
2023-03-10 18:23:44 +0000

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""compress a folder"""
+""" Function that deploys """
 from datetime import datetime
 from fabric.api import *
 import os
 import shlex
 
-env.hosts = ['34.229.149.26', '54.224.98.80']
-env.user = 'ubuntu'
+env.hosts = ['54.163.215.11', '54.234.149.233']
+env.user = "ubuntu"
+
+
+def deploy():
+    """ DEPLOYS """
+    try:
+        archive_path = do_pack()
+    except:
+        return False
+
+    return do_deploy(archive_path)
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """ For deploying """
+    """ Deploys """
     if not os.path.exists(archive_path):
         return False
     try:
@@ -37,4 +59,4 @@ def do_deploy(archive_path):
         print("New version deployed!")
         return True
     except:
-        return False`
+        return False
